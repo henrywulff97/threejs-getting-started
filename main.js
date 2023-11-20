d38fd757ed978cc306d67e7f94f2140f27311aa3
Henry Wulff <henrywulff97@gmail.com>
2023-11-20 15:46:21 +0100

    kommentare und so

--- main.js
+++ main.js
@@ -1,30 +1,26 @@
+// Benötigte Imports
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
+// Benötigte Konstanten
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
 const controls = new OrbitControls(camera, renderer.domElement);
 
+// Renderfenster erstellen und weiß färben
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
 renderer.setClearColor( 0xffffff, 0);
+document.body.appendChild( renderer.domElement );
 
+// Lichter erstellen, um Modell zu beleuchten
 const ambientLight = new THREE.AmbientLight(0xffffff, 1); // Farbe, Intensität (0-1)
 scene.add(ambientLight);
 
-/*
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-*/
-
+// Blender Datei laden
 const loader = new GLTFLoader();
-
 loader.load('/assets/Clank.glb', function ( gltf ) {
-	//console.log("Hier wird der Dude reingeladen!");
 	scene.add( gltf.scene );
 
 	// Durchlaufe alle Objekte im GLTF-Modell und gib ihre Namen aus
@@ -37,27 +33,25 @@ loader.load('/assets/Clank.glb', function ( gltf ) {
 	console.error( error );
 } );
 
+// Kamera Platzierung ("Zoom")
 camera.position.z = 5;
 
+// Animationsloop
 function animate() {
 	requestAnimationFrame( animate );
-
-	//cube.rotation.x += 0.01;
-	//cube.rotation.y += 0.01;
 
 	// Finde das Mesh im GLTF-Modell
 	const gltfMesh = scene.getObjectByName("Armature");
 
-	// Prüfe, ob das Mesh gefunden wurde
 	if (gltfMesh) {
-		// Führe Drehungen durch (Beispiel: 0.01 um x- und y-Achsen)
-		//gltfMesh.rotation.x += 0.01;
-		//gltfMesh.rotation.y += 0.01;
+		gltfMesh.rotation.y += 0.01;
 	}
 
+	// Anzeigen
 	renderer.render( scene, camera );
 }
 
+// Aktualisiere Kamera und Renderer bei Größenänderungen des Fensters
 window.addEventListener('resize', () => {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
@@ -65,4 +59,5 @@ window.addEventListener('resize', () => {
 	controls.update(); // Aktualisiere OrbitControls bei Größenänderungen
 });
 
+// Starte Animationsloop
 animate();
